abe2dfd330e65e9ff18a70bde90b146020e735d8
mate <matemorinigo@gmail.com>
2023-02-25 10:41:12 -0300

    models Project and Task created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=200, )
+    project_description=models.TextField(blank=True)
+
+    def __str__(self):
+        return self.project_name
+
+class Task(models.Model):
+    task_name=models.CharField(max_length=200)
+    task_description=models.TextField(blank=True)
+    task_project=models.ForeignKey(Project, on_delete=models.CASCADE)
+    task_isDone=models.BooleanField(default=False)
+    task_user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task_name
